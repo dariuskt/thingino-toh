ff0e95cc28041aa54dd56edce1afa3c4d38ecac2
darius <dariuskt@gmail.com>
2024-07-26 05:10:17 +0300

    first draft

---
+++ make_table.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo -e "\n### Table of Hardware\n"
+
+cat thingino-toh.yml | yq \
+'.devices[] | ""
++","+.device_type
++","+.brand
++","+.model
++","+(.status)
++","+(.soc.name)
++","+(.sensor.name)
++","+(.wifi.name)
++","+(.flash_mb // "" | tostring)
++","+(.soc.cpu_clock_mhz // "" | tostring)
++","+(.soc.ram_mb // "" | tostring)
++","+(.ethernet // "" | tostring)
++","+.wifi.w2g
++","+.data_transfer
++","+.sensor.resolution
++","+.power
++","+(.last_price_w_ship // "" | tostring)
++","+""
+' \
+| tr -d '"' | column -t -s',' -o ' | ' --table-columns \
+" ,type,brand,model,status,soc,sensor,wifi,flash MB,CPU MHz,RAM MB,ethernet,wifi,data,resolution,power,price,"
+
+#yq -y thingino-toh.yml >/dev/null
+
